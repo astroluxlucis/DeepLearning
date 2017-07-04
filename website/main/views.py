fede9df3a62580ca72cad1b70bb70b51216e43e8
exquisiteVisage <vince.doit@nwtf.org.ph>
2017-07-04 09:09:56 +0800

    Initial Commit

---
+++ website/main/views.py
@@ -0,0 +1,58 @@
+from django.contrib.auth import authenticate, login
+from django.contrib.auth import logout
+from .forms import UserForm
+from django.shortcuts import render, get_object_or_404
+from django.db.models import Q
+from django.http import JsonResponse
+
+
+def index(request):
+    if not request.user.is_authenticated():
+        return render(request, 'main/home.html')
+    else:
+        return render(request, 'main/index.html')
+
+def login_user(request):
+    if request.method == "POST":
+        username = request.POST['username']
+        password = request.POST['password']
+        user = authenticate(username=username, password=password)
+        if user is not None:
+            if user.is_active:
+                login(request, user)
+                return render(request, 'main/index.html')
+            else:
+                return render(request, 'main/login.html', {'error_message': 'Your account has been disabled'})
+        else:
+            return render(request, 'main/login.html', {'error_message': 'Invalid login'})
+    return render(request, 'main/login.html')
+
+
+def logout_user(request):
+    logout(request)
+    form = UserForm(request.POST or None)
+    context = {
+        "form": form,
+    }
+    return render(request, 'main/home.html', context)
+
+def register(request):
+    form = UserForm(request.POST or None)
+    if form.is_valid():
+        user = form.save(commit=False)
+        username = form.cleaned_data['username']
+        password = form.cleaned_data['password']
+        first_name = form.cleaned_data['first_name']
+        last_name = form.cleaned_data['last_name']
+        user.set_password(password)
+        user.save()
+        user = authenticate(username=username, password=password)
+        if user is not None:
+            if user.is_active:
+                login(request, user)
+                #albums = Album.objects.filter(user=request.user)
+                return render(request, 'main/index.html')
+    context = {
+        "form": form,
+    }
+    return render(request, 'main/register.html', context)
